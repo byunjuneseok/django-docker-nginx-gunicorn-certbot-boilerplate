b38ce034bf54594fc185da4347a92fa5b7866049
Juneseok Byun <byunjuneseok@gmail.com>
2019-08-26 11:04:44 +0900

    `start.sh`

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-diko_ss
+# ⛴django Boilerplate with nginx, gunicorn and certbot
+A boilerplate app with nginx, gunicorn and certbot(Let's encrypt).
+
+## Requirements
+* `python3-dev`, `python3-pip`
+* `docker`
+* `docker-compose`
+* `macOS` : without `certbot`
+* `linux` : `certbot` is available.
+* **"NO WINDOWS OS" 👋**
+
+## 🕹Getting Started
+To get up and running, simply do the following...
+```
+$ Please input your django project name.
+<your-project-name>
+```
+```
+$ ./start.sh
+Do you wish to use the certbot for https? [y/n]
+```
+> Just type `./start.sh` to start.
+
+```
+Do you wish to use the certbot for https? [y/n]
+n
+```
+> If you want to test this app, type `n`. You can access boilerplate app "localhost".
+
+
+```
+Do you wish to use the certbot for https? [y/n]
+y
+```
+> If you want to deploy the project on aws EC2, type `y`. Before activating certbot, you should set domain, static ip to deploy.
--- start.sh
+++ start.sh
@@ -1,11 +1,30 @@
-cd ./app
-django-admin startproject boilerplate
+#Pre-requsites
+
+
+echo "Please input your django project name."
+read projectname
+
+django-admin startproject $projectname
+mv $projectname app
+cp ./scripts/requirements.txt ./app/requirements.txt
+echo "STATIC_ROOT = os.path.join(ROOT_DIR, 'static_files')" >> ./app/$projectname/settings.py
+echo "ROOT_DIR = os.path.dirname(BASE_DIR)" >> ./app/$projectname/settings.py
 
 while true; do
-    read -p "Do you wish to use the certbot?" yn
+    read -p "Do you wish to use the certbot for https? [y/n]" yn
     case $yn in
-        [Yy]* ) sudo ./init-letsencrypt.sh; break;;
-        [Nn]* ) exit;;
+        [Yy]* ) cp ./scripts/docker-compose.yml ./docker-compose.yml;
+                mkdir nginx;
+                cp ./scripts/nginx.conf ./nginx/nginx.conf;
+                sudo ./init-letsencrypt.sh; 
+                break;;
+
+        [Nn]* ) cp ./scripts/docker-compose-wo-certbot.yml ./docker-compose.yml;
+                mkdir nginx;
+                cp ./scripts/nginx-wo-certbot.conf ./nginx/nginx.conf;
+                docker-compose up app nginx;
+                break;;
+
         * ) echo "Please answer yes or no.";;
     esac
 done
